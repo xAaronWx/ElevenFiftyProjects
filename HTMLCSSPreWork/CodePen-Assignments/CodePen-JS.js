ff015dd8df5985a22a36553a532db293f998a728
xAaronWx <50504602+xAaronWx@users.noreply.github.com>
2020-11-04 16:49:49 -0500

    JS update

---
+++ HTMLCSSPreWork/CodePen-Assignments/CodePen-JS.js
@@ -0,0 +1,17 @@
+function hello() {
+  console.log("Hello World");
+}
+
+hello();
+
+for (let i = 1; i <= 70; i *= 2) {
+  console.log(i);
+}
+
+let hero = {
+  name: "Wade Wilson",
+  alias: "Deadpool",
+  known_as: "the merc with the mouth"
+};
+
+console.log(hero.alias, "is popular for being known as", hero.known_as);
