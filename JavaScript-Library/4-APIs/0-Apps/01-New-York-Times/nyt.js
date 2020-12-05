a451c047fc771ced908c20df44cc139fb3e61695
xAaronWx <50504602+xAaronWx@users.noreply.github.com>
2020-12-05 10:26:13 -0500

    NYT Update

---
+++ JavaScript-Library/4-APIs/0-Apps/01-New-York-Times/nyt.js
@@ -0,0 +1,148 @@
+const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; // API endpoint
+const key = "7i9AkKKJGaIHsrQKJURwL9UNlKkFjMVm"; // Personal Key
+let url; // A variable
+
+// Grab references to all the DOM elements you'll need to manipulate
+
+const searchTerm = document.querySelector(".search"); // Creating a new constant by targeting the chosen selector
+const startDate = document.querySelector(".start-date");
+const endDate = document.querySelector(".end-date");
+const searchForm = document.querySelector("form"); // No dot needed for a TAG
+
+// This is never used
+// const submitBtn = document.querySelector('.submit');
+
+const nextBtn = document.querySelector(".next");
+const previousBtn = document.querySelector(".prev");
+
+const section = document.querySelector("section");
+const nav = document.querySelector("nav"); // Creating a new constant by targeting the chosen selector
+
+// Hide the "Previous"/"Next" navigation to begin with, as we don't need it immediately
+nav.style.display = "none";
+
+// define the initial page number and status of the navigation being displayed
+let pageNumber = 0;
+
+// This is never used
+// let displayNav = false;
+
+// Event listeners to control the functionality
+
+// Event listeners to control the functionality
+searchForm.addEventListener("submit", submitSearch);
+nextBtn.addEventListener("click", nextPage);
+previousBtn.addEventListener("click", previousPage);
+
+function submitSearch(e) {
+  pageNumber = 0;
+  fetchResults(e);
+}
+
+function fetchResults(e) {
+  // Use preventDefault() to stop the form submitting
+  e.preventDefault();
+
+  // Assemble the full URL
+  url =
+    baseURL +
+    "?api-key=" +
+    key +
+    "&page=" +
+    pageNumber +
+    "&q=" +
+    searchTerm.value +
+    '&fq=document_type:("article")';
+
+  if (startDate.value !== "") {
+    url += "&begin_date=" + startDate.value;
+  }
+
+  if (endDate.value !== "") {
+    url += "&end_date=" + endDate.value;
+  }
+
+  // Use fetch() to make the request to the API
+  fetch(url)
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (json) {
+      displayResults(json);
+    });
+}
+
+function displayResults(json) {
+  while (section.firstChild) {
+    // While loop will clear out any articles before a new article search is created
+    section.removeChild(section.firstChild);
+  }
+
+  const articles = json.response.docs; //Pulling articles from API
+
+  if (articles.length === 10) {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+
+  if (articles.length === 0) {
+    const para = document.createElement("p");
+    para.textContent = "No results returned.";
+    section.appendChild(para);
+  } else {
+    // This area creates the new features based on the return of a valid article
+    for (let i = 0; i < articles.length; i++) {
+      const article = document.createElement("article");
+      const heading = document.createElement("h2");
+      const link = document.createElement("a");
+      const img = document.createElement("img");
+      const para1 = document.createElement("p");
+      const para2 = document.createElement("p");
+      const clearfix = document.createElement("div");
+
+      const current = articles[i];
+      console.log(current);
+
+      link.href = current.web_url;
+      link.textContent = current.headline.main;
+      para1.textContent = current.snippet;
+      // This creates and formats the 'Keywords' from the API articles
+      para2.textContent = "Keywords: ";
+      for (let j = 0; j < current.keywords.length; j++) {
+        const span = document.createElement("span");
+        span.textContent = current.keywords[j].value + " ";
+        para2.appendChild(span);
+      }
+
+      if (current.multimedia.length > 0) {
+        img.src = "http://www.nytimes.com/" + current.multimedia[0].url;
+        img.alt = current.headline.main;
+      }
+
+      clearfix.setAttribute("class", "clearfix");
+
+      article.appendChild(heading);
+      heading.appendChild(link);
+      article.appendChild(img);
+      article.appendChild(para1);
+      article.appendChild(para2);
+      article.appendChild(clearfix);
+      section.appendChild(article);
+    }
+  }
+}
+
+function nextPage(e) {
+  pageNumber++;
+  fetchResults(e);
+}
+
+function previousPage(e) {
+  if (pageNumber > 0) {
+    pageNumber--;
+  } else {
+    return;
+  }
+  fetchResults(e);
+}
